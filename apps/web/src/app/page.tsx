2166dc3c3b4eabb5f91d36591e2b84d662a356bf
oror-sine <1staries@naver.com>
2024-10-29 20:08:16 +0900

    ➕ Feat(setup): add shadcn

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@repo/ui/shadcn/button";
+
 export default function Page(): JSX.Element {
-  return (
-    <main>
-      hello web
-    </main>
-  );
+	return (
+		<main>
+			<h1>hello web</h1>
+			<Button>Click me</Button>
+		</main>
+	);
 }
