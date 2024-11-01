9de944d5329969118f7437b954fe36f283e0b42e
Cualone <june568@naver.com>
2024-11-01 18:13:02 +0900

    ✨ feat: BaseResponse, BaseException 추가

---
+++ src/main/java/com/mulmeong/utility/common/response/BaseResponseStatus.java
@@ -0,0 +1,34 @@
+package com.mulmeong.utility.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+@AllArgsConstructor
+public enum BaseResponseStatus {
+
+    /*
+     * 응답 코드와 메시지 표준화하는 ENUM.
+     * Http 상태코드, 성공 여부, 응답 메시지, 커스텀 응답 코드, 데이터를 반환.
+     */
+
+
+    /**
+     * 200: 요청 성공.
+     **/
+    SUCCESS(HttpStatus.OK, true, 200, "요청에 성공하였습니다."),
+
+    /**
+     * 900: 기타 에러.
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, 900, "요청 처리 중 에러가 발생하였습니다.");
+
+//  이 위에 본인이 사용할 커스텀 예외를 추가해주세요.
+
+    private final HttpStatusCode httpStatusCode;
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+    }
