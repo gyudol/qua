3ef6fbb4d31f793806a5815ae70069fd1f240bf2
Gyuchan Kim <sb3g@naver.com>
2024-11-04 22:30:58 +0900

    👷 chore: Setup CI with GitHub Actions

--- src/main/java/com/mulmeong/comment/CommentApplication.java
+++ src/main/java/com/mulmeong/comment/CommentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommentApplication.class, args);
+    }
 
 }
