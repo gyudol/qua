34ba7089bf281f9845a9c0f4f3b91daae89c68e1
iqveou6 <swgm0719@naver.com>
2024-11-05 14:09:36 +0900

    ✨ feat: swagger, 응답 및 예외 처리 기능 추가

---
+++ src/main/java/com/mulmeong/comment/common/exception/BaseException.java
@@ -0,0 +1,14 @@
+package com.mulmeong.comment.common.exception;
+
+import com.mulmeong.comment.common.response.BaseResponseStatus;
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+
+    private final BaseResponseStatus status;
+
+    public BaseException(BaseResponseStatus status) {
+        this.status = status;
+    }
+}
