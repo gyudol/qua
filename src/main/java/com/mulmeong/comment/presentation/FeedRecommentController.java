6c7f1818d96b02ec1bdc9215dd78211144493132
iqveou6 <swgm0719@naver.com>
2024-11-05 15:58:12 +0900

    ✨ feat: @Operation api 설명 요약 추가

--- src/main/java/com/mulmeong/comment/presentation/FeedRecommentController.java
+++ src/main/java/com/mulmeong/comment/presentation/FeedRecommentController.java
@@ -6,6 +6,7 @@ import com.mulmeong.comment.dto.in.FeedRecommentRequestDto;
 import com.mulmeong.comment.dto.in.FeedRecommentUpdateDto;
 import com.mulmeong.comment.vo.in.FeedRecommentRequestVo;
 import com.mulmeong.comment.vo.in.FeedRecommentUpdateVo;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,7 @@ public class FeedRecommentController {
     private final FeedRecommentService feedRecommentService;
 
     @PostMapping("/comments/{commentUuid}/recomments")
+    @Operation(summary = "피드 대댓글 추가", tags = {"Feed Comment Service"})
     public BaseResponse<Void> addFeedRecomment(
             @RequestBody FeedRecommentRequestVo requestVo,
             @PathVariable String commentUuid) {
@@ -24,6 +26,7 @@ public class FeedRecommentController {
     }
 
     @PutMapping("/comments/recomments/{recommentUuid}")
+    @Operation(summary = "피드 대댓글 수정", tags = {"Feed Comment Service"})
     public BaseResponse<Void> updateFeedRecomment(
             @RequestBody FeedRecommentUpdateVo updateVo,
             @PathVariable String recommentUuid) {
@@ -32,6 +35,7 @@ public class FeedRecommentController {
     }
 
     @DeleteMapping("/comments/recomments/{recommentUuid}")
+    @Operation(summary = "피드 대댓글 삭제", tags = {"Feed Comment Service"})
     public BaseResponse<Void> deleteFeedRecomment(@PathVariable String recommentUuid) {
         feedRecommentService.deleteFeedComment(recommentUuid);
         return new BaseResponse<>();
