64564d9bd63c284e1e399f70082a5ac068268ab0
iqveou6 <swgm0719@naver.com>
2024-11-05 15:51:21 +0900

    ✨ feat: swaggerConfig 파일 수정

--- src/main/java/com/mulmeong/comment/common/config/SwaggerConfig.java
+++ src/main/java/com/mulmeong/comment/common/config/SwaggerConfig.java
@@ -1,30 +1,49 @@
 package com.mulmeong.comment.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @OpenAPIDefinition(
-        info = @io.swagger.v3.oas.annotations.info.Info(
-                title = "MULMEONG Comment Service API",
-                version = "v1",
-                description = "MULMEONG API Docs"
-        )
+        info = @Info(title = "댓글 도메인 API", version = "v1",
+                description = "피드, 쇼츠 댓글 서비스",
+                termsOfService = "http://swagger.io/terms/")
+
 )
-
-@Profile("!prod")
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
 
+    //Authorize 버튼
     @Bean
-    public GroupedOpenApi publicApi() {
-        String[] paths = {"/v1/**"};
-        return GroupedOpenApi.builder()
-                .group("public-api")
-                .pathsToMatch(paths)
-                .build();
-    }
+    public OpenAPI openApi() {
 
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components()
+                .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName));
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                // Swagger에서 요청보낼때 API에 추가되는 문자열
+                .addServersItem(new Server().url("/comment-service"));
+
+        //test
+//                .addServersItem(new Server().url("/"));
+
+    }
 }
