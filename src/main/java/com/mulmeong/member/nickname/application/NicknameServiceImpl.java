5202e56579330a078047606032fe2b4ed8800652
SeongGwang <seonggwangdev@gmail.com>
2024-11-06 16:52:21 +0900

    ✨ feat: 닉네임 조회, 수정, 중복여부 조회기능 추가

---
+++ src/main/java/com/mulmeong/member/nickname/application/NicknameServiceImpl.java
@@ -0,0 +1,58 @@
+package com.mulmeong.member.nickname.application;
+
+import com.mulmeong.member.auth.domain.Member;
+import com.mulmeong.member.auth.infrastructure.MemberRepository;
+import com.mulmeong.member.common.exception.BaseException;
+import com.mulmeong.member.common.response.BaseResponseStatus;
+import com.mulmeong.member.nickname.dto.in.CheckNicknameRequestDto;
+import com.mulmeong.member.nickname.dto.in.UpdateNicknameRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class NicknameServiceImpl implements NicknameService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 닉네임 조회.
+     *
+     * @param memberUuid 회원 uuid
+     * @return 닉네임
+     */
+    @Override
+    public String getNickname(String memberUuid) {
+        return memberRepository.findByMemberUuid(memberUuid)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_MEMBER))
+                .getNickname();
+    }
+
+    /**
+     * 닉네임 수정.
+     *
+     * @param requestDto uuid, 닉네임
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void updateNickname(UpdateNicknameRequestDto requestDto) {
+
+        Member member = memberRepository.findByMemberUuid(requestDto.getMemberUuid())
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_MEMBER));
+
+        member.updateNickname(requestDto.getNickname());
+    }
+
+    /**
+     * 닉네임 중복 검사.
+     *
+     * @param requestDto uuid, 닉네임
+     * @return 중복 여부(true: 중복 없음, false: 중복 있음)
+     */
+    @Override
+    public boolean checkNickname(CheckNicknameRequestDto requestDto) {
+
+        return !memberRepository.existsMemberByNickname(requestDto.getNickname());
+    }
+}
---
+++ src/main/java/com/mulmeong/member/nickname/presentation/NicknameController.java
@@ -0,0 +1,55 @@
+package com.mulmeong.member.nickname.presentation;
+
+import com.mulmeong.member.common.response.BaseResponse;
+import com.mulmeong.member.common.response.BaseResponseStatus;
+import com.mulmeong.member.nickname.application.NicknameService;
+import com.mulmeong.member.nickname.dto.in.CheckNicknameRequestDto;
+import com.mulmeong.member.nickname.dto.in.UpdateNicknameRequestDto;
+import com.mulmeong.member.nickname.vo.in.NicknameVo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "닉네임", description = "닉네임 관련 API")
+@RequestMapping("/v1/members")
+@RequiredArgsConstructor
+@RestController
+public class NicknameController {
+
+    private final NicknameService nicknameService;
+
+    @GetMapping("/{memberUuid}/nickname")
+    @Operation(summary = "닉네임 조회", description = "닉네임 조회 API")
+    public BaseResponse<String> getNickname(@PathVariable String memberUuid) {
+
+        return new BaseResponse<>(nicknameService.getNickname(memberUuid));
+    }
+
+    @PutMapping("/{memberUuid}/nickname")
+    @Operation(summary = "닉네임 수정", description = "닉네임 수정 API")
+    public BaseResponse<Void> updateNickname(
+            @PathVariable String memberUuid,
+            @RequestBody @Valid NicknameVo nicknameVo) {
+
+        nicknameService.updateNickname(UpdateNicknameRequestDto.toDto(
+                memberUuid, nicknameVo.getNickname()));
+
+        return new BaseResponse<>();
+    }
+
+    @PostMapping("/check-nickname")
+    @Operation(summary = "닉네임 중복 검사", description = " 변경 가능 : 200(메시지 in result) / 변경 불가 : 409(메시지 in message)")
+    public BaseResponse<Object> checkNickname(
+            @RequestBody @Valid NicknameVo nicknameVo) {
+
+
+        if (nicknameService.checkNickname(CheckNicknameRequestDto.toDto(nicknameVo.getNickname()))) {
+            return new BaseResponse<>("사용 가능한 닉네임입니다.");
+        } else {
+            return new BaseResponse<>(BaseResponseStatus.EXISTS_NICKNAME);
+        }
+    }
+
+}
