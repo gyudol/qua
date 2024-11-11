d98bb6251ac2cb007c07ef5f8f0078bdb0a755e8
iqveou6 <swgm0719@naver.com>
2024-11-11 09:27:52 +0900

    ✨ feat: 응답 및 예외 처리 기능 수정

--- src/main/java/com/mulmeong/comment/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mulmeong/comment/common/exception/GlobalExceptionHandler.java
@@ -17,21 +17,31 @@ public class GlobalExceptionHandler {
      */
 
     /**
-     * 발생한 예외 처리.
+     * BaseException 예외 처리.
      */
     @ExceptionHandler(BaseException.class)
-    protected ResponseEntity<BaseResponse<String>> handleBaseException(BaseException e) {
+    protected BaseResponse<Void> handleBaseException(BaseException e) {
 
         // 예외 메시지를 로그에 기록
-        log.error("BaseException 발생 : {}", e.getMessage());
+        log.error("BaseException 발생 : {}", e.getStatus());
 
         // 예외 스택을 로그에 기록
         for (StackTraceElement s : e.getStackTrace()) {
             System.out.println(s);
         }
 
-        BaseResponse<String> response = new BaseResponse<>(e.getStatus(), e.getMessage());
+        return new BaseResponse<>(e.getStatus());
+    }
 
-        return new ResponseEntity<>(response, response.httpStatus());
+    /**
+     * 의도치않게 발생한 RuntimeException 예외 처리
+     * 예외객체를 BaseResponse에 넘겨 응답에서 디버깅할 수 있도록 처리.
+     */
+    @ExceptionHandler(RuntimeException.class)
+    protected BaseResponse<Void> handleRuntimeException(RuntimeException e) {
+
+        log.error("런타임 예외: {}", e.getMessage());
+
+        return new BaseResponse<>(BaseResponseStatus.INTERNAL_SERVER_ERROR, e);
     }
 }
