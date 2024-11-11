ba3a7a3c8177c9f9f3059887bcb47241078811d3
iqveou6 <swgm0719@naver.com>
2024-11-11 16:13:20 +0900

    first commit

---
+++ src/main/java/com/mulmeong/contentread/ContentReadApplication.java
@@ -0,0 +1,13 @@
+package com.mulmeong.contentread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentReadApplication.class, args);
+    }
+
+}
