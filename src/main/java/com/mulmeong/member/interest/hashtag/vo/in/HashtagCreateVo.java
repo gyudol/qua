00e22dc5c2afd1623852eeeafd23f1f7d3053509
SeongGwang <seonggwangdev@gmail.com>
2024-11-11 15:10:44 +0900

    ✨ feat: 관심 해시태그 CUD 기능 구현

---
+++ src/main/java/com/mulmeong/member/interest/hashtag/vo/in/HashtagCreateVo.java
@@ -0,0 +1,12 @@
+package com.mulmeong.member.interest.hashtag.vo.in;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class HashtagCreateVo {
+
+    private List<Hashtag> hashtags;
+
+}
