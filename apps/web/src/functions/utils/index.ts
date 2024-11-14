1757b9d25e7c4bc81b47424af79ad0c06c16e39d
oror-sine <1staries@naver.com>
2024-11-14 14:02:26 +0900

    ✨ Feat: 숫자 포맷팅 유틸함수 추가

--- apps/web/src/functions/utils/index.ts
+++ apps/web/src/functions/utils/index.ts
@@ -1,4 +1,5 @@
 import alertNotImplemented from "./alertNotImplemented";
+import formatToNumAbbrs from "./formatToNumAbbrs";
 import getProfileUrl from "./getProfileUrl";
 
-export { alertNotImplemented, getProfileUrl };
+export { alertNotImplemented, getProfileUrl, formatToNumAbbrs };
