6dd859454c2482bb1ba0a13d8d52ce07755348b7
SeongGwang <seonggwangdev@gmail.com>
2024-11-16 02:08:22 +0900

    📝 docs: 헬스체크에 @Hidden 추가

--- src/main/java/com/mulmeong/admin/common/healthcheck/HealthCheckController.java
+++ src/main/java/com/mulmeong/admin/common/healthcheck/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.mulmeong.admin.common.healthcheck;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @Tag(name = "헬스 체크", description = "본 서비스가 잘 돌아가는지 확인용")
 @RequiredArgsConstructor
 @RestController
