9811510795d1baa1dd81a636ebd27db2a89e8d8c
iqveou6 <swgm0719@naver.com>
2024-11-25 09:31:23 +0900

    ✨ feat: kafka consumer config 파일 수정

--- src/main/java/com/mulmeong/comment/read/common/config/KafkaConsumerConfig.java
+++ src/main/java/com/mulmeong/comment/read/common/config/KafkaConsumerConfig.java
@@ -1,8 +1,11 @@
 package com.mulmeong.comment.read.common.config;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.mulmeong.event.*;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,232 +13,88 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${spring.kafka.consumer.group-id}")
-    private String groupId;
-
-
-    @Bean
-    public Map<String, Object> readConsumerConfigs() {
-        return CommonJsonDeserializer.getStringObjectMap(bootstrapServers, groupId);
-    }
-
-    //피드 댓글 생성
-    @Bean
-    public ConsumerFactory<String, FeedCommentCreateEvent> feedCommentCreateConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
-    }
-
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, FeedCommentCreateEvent> feedCommentCreateListener() {
-        ConcurrentKafkaListenerContainerFactory<String, FeedCommentCreateEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(feedCommentCreateConsumerFactory());
-        return factory;
-    }
-
-    //피드 댓글 수정
-    @Bean
-    public ConsumerFactory<String, FeedCommentUpdateEvent> feedCommentUpdateConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(FeedCommentCreateEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, FeedCommentUpdateEvent> feedCommentUpdateListener() {
-        ConcurrentKafkaListenerContainerFactory<String, FeedCommentUpdateEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(feedCommentUpdateConsumerFactory());
-        return factory;
-    }
-
-    //피드 댓글 삭제
-    @Bean
-    public ConsumerFactory<String, FeedCommentDeleteEvent> feedCommentDeleteConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(FeedCommentUpdateEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, FeedCommentDeleteEvent> feedCommentDeleteListener() {
-        ConcurrentKafkaListenerContainerFactory<String, FeedCommentDeleteEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(feedCommentDeleteConsumerFactory());
-        return factory;
-    }
-
-    //피드 대댓글 생성
-    @Bean
-    public ConsumerFactory<String, FeedRecommentCreateEvent> feedRecommentCreateConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(FeedCommentDeleteEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, FeedRecommentCreateEvent> feedRecommentCreateListener() {
-        ConcurrentKafkaListenerContainerFactory<String, FeedRecommentCreateEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(feedRecommentCreateConsumerFactory());
-        return factory;
-    }
-
-    //피드 대댓글 수정
-    @Bean
-    public ConsumerFactory<String, FeedRecommentUpdateEvent> feedRecommentUpdateConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(FeedRecommentCreateEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, FeedRecommentUpdateEvent> feedRecommentUpdateListener() {
-        ConcurrentKafkaListenerContainerFactory<String, FeedRecommentUpdateEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(feedRecommentUpdateConsumerFactory());
-        return factory;
-    }
-
-    //피드 대댓글 삭제
-    @Bean
-    public ConsumerFactory<String, FeedRecommentDeleteEvent> feedRecommentDeleteConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(FeedRecommentUpdateEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, FeedRecommentDeleteEvent> feedRecommentDeleteListener() {
-        ConcurrentKafkaListenerContainerFactory<String, FeedRecommentDeleteEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(feedRecommentDeleteConsumerFactory());
-        return factory;
-    }
-
-
-    //쇼츠 댓글 생성
-    @Bean
-    public ConsumerFactory<String, ShortsCommentCreateEvent> shortsCommentCreateConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(FeedRecommentDeleteEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, ShortsCommentCreateEvent> shortsCommentCreateListener() {
-        ConcurrentKafkaListenerContainerFactory<String, ShortsCommentCreateEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(shortsCommentCreateConsumerFactory());
-        return factory;
-    }
-
-    //쇼츠 댓글 수정
-    @Bean
-    public ConsumerFactory<String, ShortsCommentUpdateEvent> shortsCommentUpdateConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(ShortsCommentCreateEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, ShortsCommentUpdateEvent> shortsCommentUpdateListener() {
-        ConcurrentKafkaListenerContainerFactory<String, ShortsCommentUpdateEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(shortsCommentUpdateConsumerFactory());
-        return factory;
-    }
-
-    //쇼츠 댓글 삭제
-    @Bean
-    public ConsumerFactory<String, ShortsCommentDeleteEvent> shortsCommentDeleteConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(ShortsCommentUpdateEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, ShortsCommentDeleteEvent> shortsCommentDeleteListener() {
-        ConcurrentKafkaListenerContainerFactory<String, ShortsCommentDeleteEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(shortsCommentDeleteConsumerFactory());
-        return factory;
-    }
-
-    //쇼츠 대댓글 생성
-    @Bean
-    public ConsumerFactory<String, ShortsRecommentCreateEvent> shortsRecommentCreateConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(ShortsCommentDeleteEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, ShortsRecommentCreateEvent> shortsRecommentCreateListener() {
-        ConcurrentKafkaListenerContainerFactory<String, ShortsRecommentCreateEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(shortsRecommentCreateConsumerFactory());
-        return factory;
-    }
-
-    //쇼츠 대댓글 수정
-    @Bean
-    public ConsumerFactory<String, ShortsRecommentUpdateEvent> shortsRecommentUpdateConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(ShortsRecommentCreateEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, ShortsRecommentUpdateEvent> shortsRecommentUpdateListener() {
-        ConcurrentKafkaListenerContainerFactory<String, ShortsRecommentUpdateEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(shortsRecommentUpdateConsumerFactory());
-        return factory;
-    }
-
-    //쇼츠 대댓글 삭제
-    @Bean
-    public ConsumerFactory<String, ShortsRecommentDeleteEvent> shortsRecommentDeleteConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
+        return kafkaListenerContainerFactory(ShortsRecommentUpdateEvent.class);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, ShortsRecommentDeleteEvent> shortsRecommentDeleteListener() {
-        ConcurrentKafkaListenerContainerFactory<String, ShortsRecommentDeleteEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(shortsRecommentDeleteConsumerFactory());
+        return kafkaListenerContainerFactory(ShortsRecommentDeleteEvent.class);
+    }
+
+    @Bean
+    public <T> ConsumerFactory<String, T> consumerFactory(Class<T> messageType) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:39092,localhost:49092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.mulmeong.event");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, messageType);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public <T> ConcurrentKafkaListenerContainerFactory<String, T> kafkaListenerContainerFactory(Class<T> messageType) {
+        ConcurrentKafkaListenerContainerFactory<String, T> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory(messageType));
         return factory;
     }
-
-    //like
-    @Bean
-    public ConsumerFactory<String, LikeEvent> likeConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, LikeEvent> likeListener() {
-        ConcurrentKafkaListenerContainerFactory<String, LikeEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(likeConsumerFactory());
-        return factory;
-    }
-
-    //dislike
-    @Bean
-    public ConsumerFactory<String, DislikeEvent> dislikeConsumerFactory() {
-
-        return new DefaultKafkaConsumerFactory<>(readConsumerConfigs());
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, DislikeEvent> dislikeListener() {
-        ConcurrentKafkaListenerContainerFactory<String, DislikeEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(dislikeConsumerFactory());
-        return factory;
-    }
-
 }
