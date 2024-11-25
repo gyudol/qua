832bb03afeeb7e97096bf0d5f19eb80b98539630
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-11-25 16:31:47 +0900

    🎉 init: Create initial project structure

---
+++ src/main/java/com/mulmeong/feed/read/FeedReadApplication.java
@@ -0,0 +1,13 @@
+package com.mulmeong.feed.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedReadApplication.class, args);
+	}
+
+}
