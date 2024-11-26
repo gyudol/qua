174bbc6df564827d5b78e421a1c03d37cde5f3c8
Cualone <june568@naver.com>
2024-11-26 12:05:31 +0900

    👷 chore: Config-client 등록

--- src/main/java/com/mulmeong/contest/ContestApplication.java
+++ src/main/java/com/mulmeong/contest/ContestApplication.java
@@ -3,9 +3,11 @@ package com.mulmeong.contest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@RefreshScope
 @EnableJpaAuditing
 @EnableDiscoveryClient
 @SpringBootApplication
