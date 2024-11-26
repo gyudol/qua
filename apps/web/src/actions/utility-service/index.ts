1ed084927aa133f900a4f6399a28bad093c8d517
oror-sine <1staries@naver.com>
2024-11-26 14:47:06 +0900

    ✨ Feat: bookmark-service server-action 추가

--- apps/web/src/actions/utility-service/index.ts
+++ apps/web/src/actions/utility-service/index.ts
@@ -1,1 +1,2 @@
+export * from "./bookmarkService";
 export * from "./followService";
---
+++ apps/web/src/types/utility-service/bookmarkService.d.ts
@@ -0,0 +1,10 @@
+import type { CommonPaginationReq } from "../common";
+import type { FeedReq } from "../feed-service";
+import type { ShortsReq } from "../shorts-service";
+
+export type GetShortsBookmarksReq = CommonPaginationReq;
+export type GetFeedBookmarksReq = CommonPaginationReq;
+export type PostShortsBookmarkReq = ShortsReq;
+export type PostFeedBookmarkReq = FeedReq;
+export type DeleteShortsBookmarkReq = ShortsReq;
+export type DeleteFeedBookmarkReq = FeedReq;
