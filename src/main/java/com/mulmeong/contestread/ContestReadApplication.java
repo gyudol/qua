3033524594237bebb7a6c36027eec223a448a84a
Cualone <june568@naver.com>
2024-11-27 12:30:59 +0900

    first commit

---
+++ src/main/java/com/mulmeong/contestread/ContestReadApplication.java
@@ -0,0 +1,14 @@
+package com.mulmeong.contestread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class ContestReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContestReadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mulmeong/contestread/ContestReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulmeong.contestread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
