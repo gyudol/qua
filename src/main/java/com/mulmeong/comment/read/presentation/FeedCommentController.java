c157452431e530404f4e909dfe735374b8679408
iqveou6 <swgm0719@naver.com>
2024-11-27 11:36:45 +0900

    ✨ feat: uri 수정

--- src/main/java/com/mulmeong/comment/read/presentation/FeedCommentController.java
+++ src/main/java/com/mulmeong/comment/read/presentation/FeedCommentController.java
@@ -20,7 +20,7 @@ public class FeedCommentController {
 
     private final FeedCommentService feedCommentService;
 
-    @GetMapping("/comment/{feedUuid}/comments")
+    @GetMapping("/{feedUuid}/comments")
     @Operation(summary = "피드 댓글 최신순 , 추천순 조회", tags = {"Feed Comment Service"})
     public BaseResponse<CursorPage<FeedCommentResponseVo>> getFeedCommentsByPage(
             @PathVariable String feedUuid,
