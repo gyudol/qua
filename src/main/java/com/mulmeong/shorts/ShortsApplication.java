0f095588f255a0134c176a748a88f747c6bc7022
Gyuchan Kim <sb3g@naver.com>
2024-12-04 15:30:23 +0900

    🎉 init: Create initial project structure

---
+++ src/main/java/com/mulmeong/shorts/ShortsApplication.java
@@ -0,0 +1,13 @@
+package com.mulmeong.shorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortsApplication.class, args);
+	}
+
+}
