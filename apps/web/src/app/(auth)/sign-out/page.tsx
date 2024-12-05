0c6b889886434a96c5101a6a7629719dc5a517d2
oror-sine <1staries@naver.com>
2024-12-05 22:33:50 +0900

    ♻️ Chore: 로그아웃 버튼 클릭하기 쉽게 수정

--- apps/web/src/app/(auth)/sign-out/page.tsx
+++ apps/web/src/app/(auth)/sign-out/page.tsx
@@ -5,7 +5,7 @@ import { CommonLayout } from "@/components/common/atoms";
 
 export default function LogoutButton() {
   return (
-    <CommonLayout.Contents>
+    <CommonLayout.Contents className="min-h-screen flex justify-center items-center">
       <button onClick={() => void signOut({ callbackUrl: "/" })} type="button">
         로그아웃
       </button>
