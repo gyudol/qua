7d55cdb91df8226c5f695d5fb030363ef7652605
oror-sine <1staries@naver.com>
2024-12-05 17:21:18 +0900

    💄 Style: button 아이콘 변경

--- apps/web/src/components/feed/molecules/FeedMoreOption.tsx
+++ apps/web/src/components/feed/molecules/FeedMoreOption.tsx
@@ -4,7 +4,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@repo/ui/shadcn/dropdown-menu";
-import { MoreVertical } from "lucide-react";
+import { MoreHorizontal } from "lucide-react";
 import { useSessionContext } from "@/context/SessionContext";
 import type { Feed } from "@/types/feed/feed-read-service";
 import { FeedReportButton } from "../atoms/FeedReportButton";
@@ -20,7 +20,7 @@ export function FeedMoreOption({ feedUuid, memberUuid }: FeedMoreOptionProps) {
     <DropdownMenu>
       <DropdownMenuTrigger>
         <button type="button" className="p-1 hover:bg-gray-100 rounded-full">
-          <MoreVertical className="w-5 h-5" />
+          <MoreHorizontal className="w-5 h-5" />
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
