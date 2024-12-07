086c6d8340f9d9d1c46fe9172dde24a78bd1ed21
oror-sine <1staries@naver.com>
2024-12-07 21:33:22 +0900

    🔧 Chore: 이미지 도메인 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
   images: {
-    domains: ["media.qua.world"],
+    domains: ["media.qua.world", "qua-assets.s3.ap-northeast-2.amazonaws.com"],
   },
 };
