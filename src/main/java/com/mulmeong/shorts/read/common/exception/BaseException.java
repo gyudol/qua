a7a31f34c783af0850b2143d968aee4cdf191102
Gyuchan Kim <sb3g@naver.com>
2024-12-08 19:46:35 +0900

    ✨ feat: Define core components for API setup and error handling

---
+++ src/main/java/com/mulmeong/shorts/read/common/exception/BaseException.java
@@ -0,0 +1,14 @@
+package com.mulmeong.shorts.read.common.exception;
+
+import com.mulmeong.shorts.read.common.response.BaseResponseStatus;
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+
+    private final BaseResponseStatus status;
+
+    public BaseException(BaseResponseStatus status) {
+        this.status = status;
+    }
+}
