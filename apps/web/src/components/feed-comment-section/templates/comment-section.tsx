b35d7c9f14eb313acf7f82ed939f9ecfe442a46d
oror-sine <1staries@naver.com>
2024-12-09 00:14:05 +0900

    ✨ Feat: 댓글창이 비어있는 경우 처리

--- apps/web/src/components/feed-comment-section/templates/comment-section.tsx
+++ apps/web/src/components/feed-comment-section/templates/comment-section.tsx
@@ -24,22 +24,29 @@ export function CommentSection({ feedUuid }: CommentSectionProps) {
   });
 
   return (
-    <section id="comment" className="flex flex-col min-h-[50vh] p-[1rem]">
+    <section id="comment" className="flex flex-col min-h-[10rem] p-[1rem]">
       <div className="mb-[1rem]">
         <CommentInput {...{ feedUuid }} />
       </div>
-      <div>
-        {newCommentList?.map((comment) => (
-          <CommentView key={comment.commentUuid} {...comment} justNow />
-        ))}
-        {data?.pages.map((page) => (
-          <React.Fragment key={page.pageNo}>
-            {page.content.map((comment) => (
-              <CommentView key={comment.commentUuid} {...comment} />
-            ))}
-          </React.Fragment>
-        ))}
-      </div>
+      {newCommentList?.length || data?.pages[0].content.length ? (
+        <div>
+          {newCommentList?.map((comment) => (
+            <CommentView key={comment.commentUuid} {...comment} justNow />
+          ))}
+          {data?.pages.map((page) => (
+            <React.Fragment key={page.pageNo}>
+              {page.content.map((comment) => (
+                <CommentView key={comment.commentUuid} {...comment} />
+              ))}
+            </React.Fragment>
+          ))}
+        </div>
+      ) : (
+        <div className="w-full flex-1 flex justify-center items-center text-teal-400 font-bold">
+          첫번째 댓글을 작성해보세요!
+        </div>
+      )}
+
       <div ref={observerRef}>
         {isFetchingNextPage ? <p>loading more...</p> : null}
       </div>
