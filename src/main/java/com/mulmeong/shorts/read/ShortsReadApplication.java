9ab696354926368f07111b18248d4419c0fe833a
Gyuchan Kim <sb3g@naver.com>
2024-12-08 17:15:10 +0900

    🎉 init: Create initial project structure

---
+++ src/main/java/com/mulmeong/shorts/read/ShortsReadApplication.java
@@ -0,0 +1,13 @@
+package com.mulmeong.shorts.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortsReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortsReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mulmeong/shorts/read/ShortsReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulmeong.shorts.read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortsReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
