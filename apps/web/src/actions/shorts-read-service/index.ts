cc38280a03c81e26d319f7aae4d6b3c3e0889245
oror-sine <1staries@naver.com>
2024-12-09 11:49:50 +0900

    ✨ Feat: shorts-read-service 타입 및 서버액션 정의

---
+++ apps/web/src/actions/shorts-read-service/index.ts
@@ -0,0 +1,68 @@
+"use server";
+
+import type {
+  GetMemberShortsesReq,
+  GetMemberShrotsRecsReq,
+  GetShortsRecsReq,
+  GetShortsReq,
+  Shorts,
+} from "@/types/shorts/shorts-read-service";
+import { toURLSearchParams } from "@/functions/utils";
+import { getHeaders, processResponse } from "../common";
+
+const API_SERVER = process.env.BASE_API_URL;
+const PREFIX = "shorts-read-service";
+
+export async function getShorts({ shortsUuid }: GetShortsReq) {
+  const URI = `${API_SERVER}/${PREFIX}/v1/shorts/${shortsUuid}`;
+
+  const res: Response = await fetch(URI, {
+    headers: await getHeaders(),
+    method: "GET",
+    cache: "no-cache",
+  });
+
+  return processResponse<Shorts, false>({ res });
+}
+
+export async function getShortsRecs({ ...query }: GetShortsRecsReq) {
+  const URI = `${API_SERVER}/${PREFIX}/v1/shorts/recommendation?${toURLSearchParams(query)}`;
+
+  const res: Response = await fetch(URI, {
+    headers: await getHeaders(),
+    method: "GET",
+    cache: "no-cache",
+  });
+
+  return processResponse<Shorts, true>({ res });
+}
+
+export async function getMemberShortses({
+  memberUuid,
+  ...query
+}: GetMemberShortsesReq) {
+  const URI = `${API_SERVER}/${PREFIX}/v1/members/${memberUuid}/shorts?${toURLSearchParams(query)}`;
+
+  const res: Response = await fetch(URI, {
+    headers: await getHeaders(),
+    method: "GET",
+    cache: "no-cache",
+  });
+
+  return processResponse<Shorts, true>({ res });
+}
+
+export async function getMemberShortsRecs({
+  memberUuid,
+  ...query
+}: GetMemberShrotsRecsReq) {
+  const URI = `${API_SERVER}/${PREFIX}/auth/v1/members/${memberUuid}/shorts/recommendation?${toURLSearchParams(query)}`;
+
+  const res: Response = await fetch(URI, {
+    headers: await getHeaders(),
+    method: "GET",
+    cache: "no-cache",
+  });
+
+  return processResponse<Shorts, true>({ res });
+}
