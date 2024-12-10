6bd0bec858fe316da85aa40919ac567945899637
oror-sine <1staries@naver.com>
2024-12-10 18:59:43 +0900

    ✨ Feat: member-feed api 서버액션 및 훅 정의

--- apps/web/src/hooks/feed-read-service/index.ts
+++ apps/web/src/hooks/feed-read-service/index.ts
@@ -1,8 +1,11 @@
 "use client";
 
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import { getFeed, getFeeds } from "@/actions/feed-read-service";
-import type { GetFeedsReq } from "@/types/feed/feed-read-service";
+import { getFeed, getFeeds, getMemberFeeds } from "@/actions/feed-read-service";
+import type {
+  GetFeedsReq,
+  GetMemberFeeds,
+} from "@/types/feed/feed-read-service";
 import type { FeedReq } from "@/types/feed/common";
 
 export function useGetFeedsInfiniteQuery({ ...query }: GetFeedsReq) {
@@ -45,5 +48,34 @@ export function useGetFeedQuery({ feedUuid }: FeedReq) {
       },
     ],
     queryFn: () => getFeed({ feedUuid }),
+  });
+}
+
+export function useGetMemberFeedsInfiniteQuery({ ...query }: GetMemberFeeds) {
+  const { memberUuid, sortBy, pageNo, pageSize, nextCursor } = query;
+  return useInfiniteQuery({
+    queryKey: [
+      "feed-service",
+      {
+        kind: "member-feeds",
+        query,
+      },
+    ],
+    queryFn: ({ pageParam }) =>
+      getMemberFeeds({ memberUuid, sortBy, ...pageParam }),
+    getNextPageParam: ({
+      hasNext,
+      ...nextQuery
+    }: {
+      pageNo: number;
+      pageSize: number;
+      nextCursor: string;
+      hasNext: boolean;
+    }) => (hasNext ? { ...nextQuery } : null),
+    initialPageParam: {
+      pageNo: pageNo || 1,
+      pageSize: pageSize || 10,
+      nextCursor: nextCursor || undefined,
+    },
   });
 }
