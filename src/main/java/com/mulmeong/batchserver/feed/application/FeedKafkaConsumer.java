77c51df3de3b4d89b2186dba6849221558946daa
Cualone <june568@naver.com>
2024-12-10 16:19:54 +0900

    ✨ feat: listener 위치 변경, kind에 따라 분기 처리

--- src/main/java/com/mulmeong/batchserver/feed/application/FeedKafkaConsumer.java
+++ src/main/java/com/mulmeong/batchserver/feed/application/FeedKafkaConsumer.java
@@ -1,7 +1,5 @@
 package com.mulmeong.batchserver.feed.application;
 
-import com.mulmeong.batchserver.utility.application.FollowService;
-import com.mulmeong.event.utility.consume.FeedCreateEvent;
 import com.mulmeong.event.utility.consume.LikesCreateEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,13 +11,6 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class FeedKafkaConsumer {
 
-    private final FeedService feedService;
 
-    @KafkaListener(topics = "${event.utility.pub.topics.like-create.name}",
-            containerFactory = "likeCreateListener")
-    public void feedCreated(LikesCreateEvent message) {
-        log.info("feed create message: {}", message.getKindUuid());
-        feedService.likeCountRenew(message);
-    }
 
 }
--- src/main/java/com/mulmeong/batchserver/utility/application/UtilityKafkaConsumer.java
+++ src/main/java/com/mulmeong/batchserver/utility/application/UtilityKafkaConsumer.java
@@ -1,6 +1,8 @@
 package com.mulmeong.batchserver.utility.application;
 
+import com.mulmeong.batchserver.feed.application.FeedService;
 import com.mulmeong.event.utility.consume.FeedCreateEvent;
+import com.mulmeong.event.utility.consume.LikesCreateEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,12 +14,31 @@ import org.springframework.stereotype.Service;
 public class UtilityKafkaConsumer {
 
     private final FollowService followService;
+    private final FeedService feedService;
 
     @KafkaListener(topics = "${event.feed.pub.topics.feed-create.name}",
             containerFactory = "feedCreateListener")
     public void feedCreated(FeedCreateEvent message) {
         log.info("feed create message: {}", message.getFeedUuid());
         followService.createFeedFollowerAlert(message);
+    }
+
+    @KafkaListener(topics = "${event.utility.pub.topics.like-create.name}",
+            containerFactory = "likeCreateListener")
+    public void feedLikesCreated(LikesCreateEvent message) {
+        String kind = message.getKind();
+        log.info("like message: {}", kind);
+        switch (kind) {
+            case "FEED":
+                feedService.likeCountRenew(message);
+                break;
+            case "SHORTS":
+                break;
+            case "COMMENT":
+                break;
+            case "RECOMMENT":
+                break;
+        }
     }
 
 }
