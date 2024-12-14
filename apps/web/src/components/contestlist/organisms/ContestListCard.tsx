7b1d9998a514456dd1b11b4ebe62a9ab553997ae
eomhyunsik <hyunsik2005@gmail.com>
2024-12-15 04:28:50 +0900

    ✨ Feat: 포스트 리스트 api 연동

--- apps/web/src/components/contestlist/organisms/ContestListCard.tsx
+++ apps/web/src/components/contestlist/organisms/ContestListCard.tsx
@@ -2,52 +2,46 @@
 import React from "react";
 import Image from "next/image";
 import type { ContestPostList } from "@/types/contest/contest";
-import ContestFilter from "../atoms/ContestFilter";
 
 function ContestCard({ media, createdAt, voteCount }: ContestPostList) {
   return (
-    <div className="min-h-screen bg-gray-100 pt-5 px-4 border-2">
-      <ContestFilter />
-      <div className="grid grid-cols-2 gap-4 w-full">
-        <div className="flex flex-col">
-          <div className="relative h-[200px] overflow-hidden round-t-lg">
-            <div className="bg-slate-300 w-full h-full">
-              {media.mediaType === "IMAGE" ? (
-                <Image
-                  src={media.assets.mediaUrl}
-                  alt={media.assets.description || "Contest Image"}
-                  className="w-full h-full object-cover rounded-lg"
-                  width={300}
-                  height={300}
-                />
-              ) : (
-                <video
-                  className="w-full h-full object-cover rounded-lg"
-                  controls
-                  src={media.assets.mediaUrl}
-                >
-                  <track
-                    kind="captions"
-                    src="path_to_captions.vtt"
-                    srcLang="en"
-                    label="English"
-                    default
-                  />
-                </video>
-              )}
-            </div>
-          </div>
-          <button
-            type="button"
-            className="py-2 border-2 border-solid rounded-b-lg round-t-lg bg-[#47D0BF] text-white"
-          >
-            투표하기
-          </button>
-          <div className="flex justify-between items-center mt-1 text-sm text-gray-500 rounded-t-2xl">
-            <span className="text-[0.65rem]">{createdAt}</span>
-            <span className="text-[0.65rem]">투표수: {voteCount}</span>
-          </div>
+    <div className="flex flex-col">
+      <div className="relative h-[200px] overflow-hidden round-t-lg">
+        <div className="bg-slate-300 w-full h-full">
+          {media.mediaType === "IMAGE" ? (
+            <Image
+              src={media.assets.mediaUrl}
+              alt={media.assets.description || "Contest Image"}
+              className="w-full h-full object-cover rounded-lg"
+              width={300}
+              height={300}
+            />
+          ) : (
+            <video
+              className="w-full h-full object-cover rounded-lg"
+              controls
+              src={media.assets.mediaUrl}
+            >
+              <track
+                kind="captions"
+                src="path_to_captions.vtt"
+                srcLang="en"
+                label="English"
+                default
+              />
+            </video>
+          )}
         </div>
+      </div>
+      <button
+        type="button"
+        className="py-2 border-2 border-solid rounded-b-lg round-t-lg bg-[#47D0BF] text-white"
+      >
+        투표하기
+      </button>
+      <div className="flex justify-between items-center mt-1 text-sm text-gray-500 rounded-t-2xl">
+        <span className="text-[0.65rem]">{createdAt}</span>
+        <span className="text-[0.65rem]">투표수: {voteCount}</span>
       </div>
     </div>
   );
--- apps/web/src/components/contestlist/templates/ContestList.tsx
+++ apps/web/src/components/contestlist/templates/ContestList.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useInfiniteScroll } from "@/hooks";
 import { useGetConstestListInfiniteQuery } from "@/hooks/contest-list-service";
 import ContestCard from "../organisms/ContestListCard";
+import ContestFilter from "../atoms/ContestFilter";
 
 export default function ContestList({ contestId }: { contestId: number }) {
   const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
@@ -18,28 +19,31 @@ export default function ContestList({ contestId }: { contestId: number }) {
   // console.log("콘테스트 id", contestId);
 
   return (
-    <div>
-      {data
-        ? data.pages.map((page) =>
-            page.content.map(
-              ({ media, createdAt, voteCount, postUuid, memberUuid }) => (
-                <ContestCard
-                  key={postUuid}
-                  {...{
-                    contestId,
-                    media,
-                    createdAt,
-                    voteCount,
-                    memberUuid,
-                    postUuid,
-                  }}
-                />
+    <div className="min-h-screen bg-gray-100 pt-5 px-4 border-2">
+      <ContestFilter />
+      <div className="grid grid-cols-2 gap-4 w-full">
+        {data
+          ? data.pages.map((page) =>
+              page.content.map(
+                ({ media, createdAt, voteCount, postUuid, memberUuid }) => (
+                  <ContestCard
+                    key={postUuid}
+                    {...{
+                      contestId,
+                      media,
+                      createdAt,
+                      voteCount,
+                      memberUuid,
+                      postUuid,
+                    }}
+                  />
+                ),
               ),
-            ),
-          )
-        : null}
-      <div ref={observerRef} className="">
-        {isFetchingNextPage ? "로딩중" : null}
+            )
+          : null}
+        <div ref={observerRef} className="">
+          {isFetchingNextPage ? "로딩중" : null}
+        </div>
       </div>
     </div>
   );
