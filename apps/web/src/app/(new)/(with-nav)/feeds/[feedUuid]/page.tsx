cc1a85abf8bbc39c4783ee1e8e35eb2b347b08a1
oror-sine <1staries@naver.com>
2024-12-17 13:57:30 +0900

    🚨 Chore: lint

--- apps/web/src/app/(new)/(with-nav)/feeds/[feedUuid]/page.tsx
+++ apps/web/src/app/(new)/(with-nav)/feeds/[feedUuid]/page.tsx
@@ -1,6 +1,4 @@
-import { Separator } from "@repo/ui/shadcn/separator";
 import PageContainer from "@/components/@new/layouts/containers/PageContainer";
-import { CommentSection } from "@/components/feed-comment-section/templates";
 import { FeedDetailSection } from "@/components/feed/templates/FeedDetailSection";
 import CommentDrawer from "@/components/feed-comment-section/organisms/CommentDrawer";
 
@@ -14,9 +12,6 @@ export default function page({ params: { feedUuid } }: PageProps) {
   return (
     <PageContainer>
       <FeedDetailSection {...{ feedUuid }} />
-      {/* <Separator className="bg-zinc-200 h-2" /> */}
-      {/* <CommentSection {...{ feedUuid }} />
-       */}
       <CommentDrawer {...{ feedUuid }} />
     </PageContainer>
   );
