5d4478ada33dfc45dbf31e6f25bfdec383dc76e1
oror-sine <1staries@naver.com>
2024-12-17 14:51:19 +0900

    💄 Style: 스타일링

--- apps/web/src/app/(new)/(without-nav)/feeds/[feedUuid]/page.tsx
+++ apps/web/src/app/(new)/(without-nav)/feeds/[feedUuid]/page.tsx
@@ -1,6 +1,7 @@
 import PageContainer from "@/components/@new/layouts/containers/PageContainer";
 import { FeedDetailSection } from "@/components/feed/templates/FeedDetailSection";
 import CommentDrawer from "@/components/feed-comment-section/organisms/CommentDrawer";
+import { FeedCommentDrawerContextProvider } from "@/provider/FeedCommnetDrawerContextProvider";
 
 interface PageProps {
   params: {
@@ -10,9 +11,11 @@ interface PageProps {
 
 export default function page({ params: { feedUuid } }: PageProps) {
   return (
-    <PageContainer>
-      <FeedDetailSection {...{ feedUuid }} />
+    <FeedCommentDrawerContextProvider>
+      <PageContainer>
+        <FeedDetailSection {...{ feedUuid }} />
+      </PageContainer>
       <CommentDrawer {...{ feedUuid }} />
-    </PageContainer>
+    </FeedCommentDrawerContextProvider>
   );
 }
