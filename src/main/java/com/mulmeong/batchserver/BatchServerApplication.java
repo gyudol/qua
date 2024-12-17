44d8f5b6041bd084afdf8e99c527ec1fe272d4d3
Cualone <june568@naver.com>
2024-12-17 13:43:30 +0900

    ✨ feat: 유레카 연결

--- src/main/java/com/mulmeong/batchserver/BatchServerApplication.java
+++ src/main/java/com/mulmeong/batchserver/BatchServerApplication.java
@@ -3,10 +3,12 @@ package com.mulmeong.batchserver;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableBatchProcessing
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BatchServerApplication {
 
